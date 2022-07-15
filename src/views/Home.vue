bf84ee25166a24ccdabb049d8fa217f482c13622
andriferry <ferryandri35@gmail.com>
2022-07-16 06:05:04 +0700

    Add fullcalendar

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,26 @@
 <template>
-    <div class="home">Hello</div>
+    <div class="home">
+        <FullCalendar :options="calendarOptions" />
+    </div>
 </template>
 
 <script>
+import FullCalendar from '@fullcalendar/vue';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
+
 export default {
     name: 'Home',
+    components: {
+        FullCalendar,
+    },
+    data() {
+        return {
+            calendarOptions: {
+                plugins: [dayGridPlugin, interactionPlugin],
+                initialView: 'dayGridMonth',
+            },
+        };
+    },
 };
 </script>
