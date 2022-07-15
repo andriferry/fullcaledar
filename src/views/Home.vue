a859a17c6505cacd0a6ba98a71955a2644484473
andriferry <ferryandri35@gmail.com>
2022-07-16 06:11:32 +0700

    Update event

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -19,8 +19,14 @@ export default {
             calendarOptions: {
                 plugins: [dayGridPlugin, interactionPlugin],
                 initialView: 'dayGridMonth',
+                events: [
+                    {title: 'event 1', date: '2022-07-02'},
+                    {title: 'event 2', date: '2022-07-30'},
+                ],
             },
         };
     },
 };
 </script>
+
+<style scoped></style>
