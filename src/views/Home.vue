906772be056a0c6b078b75cf66dd7d378a7e0bd9
andriferry <ferryandri35@gmail.com>
2022-07-16 08:45:09 +0700

    add tooltips component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,30 +1,6 @@
 <template>
     <div class="home">
         <FullCalendar :options="calendarOptions" />
-
-        <div class="tooltip" style="">
-            <div class="title">
-                <p class="title-event">Casual leave</p>
-                <p class="day">1 DAY</p>
-                <p class="status">personal</p>
-            </div>
-            <div class="user-container">
-                <div class="user">
-                    <div class="avatar-container">
-                        <div class="avatar">
-                            <p class="" style="margin: 0px">JH</p>
-                        </div>
-                    </div>
-
-                    <div class="user-info">
-                        <p class="user-name" style="margin: 0px">
-                            Janitha Harischandra
-                        </p>
-                        <p class="user-task" style="margin: 0px">ArcticHare</p>
-                    </div>
-                </div>
-            </div>
-        </div>
     </div>
 </template>
 
@@ -35,6 +11,7 @@ import interactionPlugin from '@fullcalendar/interaction';
 import tippy from 'tippy.js';
 import 'tippy.js/dist/tippy.css'; // optional for styling
 import 'tippy.js/animations/scale.css';
+import Tooltips from '../components/Tooltips.vue';
 
 export default {
     name: 'Home',
@@ -48,8 +25,9 @@ export default {
 
                 eventDidMount(info) {
                     tippy(info.el, {
-                        content: info.event.title,
+                        content: Tooltips,
                         animation: 'fade',
+                        allowHTML: true,
                     });
                 },
                 initialView: 'dayGridMonth',
@@ -70,98 +48,3 @@ export default {
     },
 };
 </script>
-
-<style scoped>
-.tooltip {
-    width: 300px;
-    background-color: white;
-    box-shadow: 10px;
-    margin: 20px;
-    color: white;
-    min-height: 100px;
-    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-}
-
-.tooltip .title {
-    padding: 20px;
-    /* display: flex;
-
-    justify-content: start; */
-}
-
-.tooltip .title .title-event {
-    color: teal;
-    text-align: start;
-    text-transform: uppercase;
-    font-weight: 900;
-    margin: 0px;
-}
-.tooltip .title .day {
-    color: rgb(52, 49, 49);
-    text-align: start;
-    text-transform: uppercase;
-    font-weight: 500;
-    font-size: 20px;
-    margin: 3px 0px;
-}
-
-.tooltip .title .status {
-    color: gray;
-    text-align: start;
-    font-weight: 700;
-    font-size: 13px;
-    margin: 0px;
-}
-
-.tooltip .user-container {
-    background: #f5f5f5;
-}
-
-.user-container .user {
-    display: flex;
-    color: rgb(52, 49, 49);
-    gap: 1rem;
-    margin: 0px 20px;
-    height: 60px;
-}
-.user .avatar-container {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-content: center;
-}
-
-.user-container .user .avatar-container .avatar {
-    width: 50px;
-    height: 50px;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-content: center;
-    background: teal;
-    border-radius: 50%;
-    color: white;
-    font-size: 20px;
-    font-weight: 700;
-}
-
-.user .user-info {
-    display: flex;
-    flex-direction: column;
-    align-content: center;
-    justify-content: center;
-}
-
-.user .user-info p {
-    text-align: start;
-}
-
-.user .user-info .user-name {
-    color: rgb(52, 49, 49);
-    font-weight: 600;
-}
-.user .user-task {
-    color: grey;
-    font-weight: 500;
-}
-</style>
